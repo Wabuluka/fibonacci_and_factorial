082efd5a18821bb02f3e808b43da17f9719f6234
Wabuluka <dwabuluka@gmail.com>
2020-10-05 03:51:52 +0300

    eslint rule edited, added and applied on save

--- fibonacci.js
+++ fibonacci.js
@@ -1,14 +1,22 @@
 /**
+
  * Group Members
+
  * Walter Ruganzu
+
  * Mike Mwanje
+
  * Isaac Ssemugenyi
+
  * Davies Wabuluka
- * 
+
+ *
+
  * Fibonacci Numbers = Are numbers where the preceeding number
+
  * is the sum of two previous fibonacci numbers
+
 */
-const calculator = require('./calculator');
 
 function fibonacci(number) {
   return number < 0 ? 0 : number < 2 ? number : fibonacci(number - 1) + fibonacci(number - 2);
